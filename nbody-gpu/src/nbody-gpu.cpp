c2c392c47e248bc35df305c66df37e13e1dd1094
Finn Else-McCormick <2200864@uad.ac.uk>
2024-04-25 08:35:30 +0100

    Initial commit

---
+++ nbody-gpu/src/nbody-gpu.cpp
@@ -0,0 +1,32 @@
+#include <sycl/sycl.hpp>
+#include <iostream>
+
+using namespace sycl;
+
+// Create an exception handler for asynchronous SYCL exceptions
+static auto exception_handler = [](sycl::exception_list e_list) {
+	for (std::exception_ptr const &e : e_list) {
+		try {
+			std::rethrow_exception(e);
+		}
+		catch (std::exception const &e) {
+			std::terminate();
+		}
+	}
+};
+
+int main(int argc, char* argv[]) {
+
+	auto selector = default_selector_v;
+
+	try {
+		queue q(selector, exception_handler);
+
+		// Print out the device information used for the kernel code.
+		std::cout << "Running on device: " << q.get_device().get_info<info::device::name>() << "\n";
+	} catch (exception const &e) {
+		std::terminate();
+	}
+
+	return 0;
+}
